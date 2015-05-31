860134bb3324b4acb0b89dd8af1700a34d689b05
Michael Aquilina <michaelaquilina@gmail.com>
2015-05-31 01:04:03 +0100

    Add runtime information to problem1.go

--- problem1.go
+++ problem1.go
@@ -1,13 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	start := time.Now()
+
 	total := 0
 	for i := 0; i < 1000; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			total += i
 		}
 	}
+
+	elapsed := time.Since(start)
+
 	fmt.Println("Result:", total)
+	fmt.Println("Runtime:", elapsed)
 }
